f309dadfb84b3838347dcb46fefb0cf14d437986
Samuel Cornet <samuel@noukod.com>
2019-01-29 15:56:26 +0000

    Automatically backed up by Learn

--- app/controllers/captains_controller.rb
+++ app/controllers/captains_controller.rb
@@ -1,3 +1,27 @@
 class CaptainsController < ApplicationController
+  has_many :boats
+  has_many :classifications, through: :boats
+
+  def self.catamaran_operators
+    includes(boats: :classifications)
+      .where(classifications: {name: 'Catamaran'})
+  end
+
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: 'Sailboat'}).uniq
+  end
+
+  def self.motors
+    includes(boats: :classifications).where(classifications: {name: 'Motorboat'})
+  end
+
+  def self.talented_seamen
+    where("id IN (?)", sailors.pluck(:id) & motors.pluck(:id))
+  end
+
+  def self.non_sailors
+    where("id NOT IN (?)", sailors.pluck(:id))
+  end
+
 
 end
