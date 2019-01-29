29d40115d64ee2fae33616b0ed8b9720e7658cca
Samuel Cornet <samuel@noukod.com>
2019-01-29 16:36:13 +0000

    Automatically backed up by Learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -18,7 +18,7 @@ class Boat < ActiveRecord::Base
     end
 
     def self.last_three_alphabetically
-      order(name: :desc).limit(3)
+      # order(name: :desc).limit(3)
       where(Boat.arel_table.order.desc.limit(3))
     end
 
