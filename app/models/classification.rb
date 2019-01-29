4d3b2ee628467f1af9676403214328ba35e13b87
Samuel Cornet <samuel@noukod.com>
2019-01-29 15:58:36 +0000

    Automatically backed up by Learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,9 +2,6 @@ class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
 
-  has_many :boat_classifications
-  has_many :boats, through: :boat_classifications
-
   def self.my_all
     uniq
   end
